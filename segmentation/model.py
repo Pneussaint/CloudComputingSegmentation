5b889bed0a200dac185f4af846d2c029b6041409
neussaint.pablo <pablo.neussaint@gmail.com>
2024-04-01 20:21:48 +0200

    model fonctionnel

---
+++ segmentation/model.py
@@ -0,0 +1,73 @@
+import random
+
+import cv2
+import numpy as np
+import torchvision
+import torchvision.transforms as T
+from torchvision.models.detection import MaskRCNN_ResNet50_FPN_Weights
+
+COCO_INSTANCE_CATEGORY_NAMES = [
+    '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
+    'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+    'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
+    'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+    'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+    'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+    'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
+    'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+    'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
+    'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+]
+
+model = torchvision.models.detection.maskrcnn_resnet50_fpn(
+    weights=MaskRCNN_ResNet50_FPN_Weights.DEFAULT)
+model.eval()
+
+def get_weights():
+    return MaskRCNN_ResNet50_FPN_Weights.DEFAULT
+
+def random_colour_masks(image):
+    colours = [[0, 255, 0], [0, 0, 255], [255, 0, 0], [0, 255, 255], [255, 255, 0], [
+        255, 0, 255], [80, 70, 180], [250, 80, 190], [245, 145, 50], [70, 150, 250], [50, 190, 190]]
+    r = np.zeros_like(image).astype(np.uint8)
+    g = np.zeros_like(image).astype(np.uint8)
+    b = np.zeros_like(image).astype(np.uint8)
+    r[image == 1], g[image == 1], b[image ==
+                                    1] = colours[random.randrange(0, 10)]
+    coloured_mask = np.stack([r, g, b], axis=2)
+    return coloured_mask
+
+
+def get_prediction(img, threshold):
+    transform = T.Compose([T.ToTensor()])
+    img = transform(img)
+    pred = model([img])
+    pred_score = list(pred[0]['scores'].detach().numpy())
+    pred_t = [pred_score.index(x) for x in pred_score if x > threshold][-1]
+    masks = (pred[0]['masks'] > 0.5).squeeze().detach().cpu().numpy()
+    pred_class = [COCO_INSTANCE_CATEGORY_NAMES[i]
+                  for i in list(pred[0]['labels'].numpy())]
+    pred_boxes = [[(i[0], i[1]), (i[2], i[3])]
+                  for i in list(pred[0]['boxes'].detach().numpy())]
+    masks = masks[:pred_t+1]
+    pred_boxes = pred_boxes[:pred_t+1]
+    pred_class = pred_class[:pred_t+1]
+    return masks, pred_boxes, pred_class
+
+
+def segmentation(img_path, threshold=0.5, rect_th=3, text_size=1, text_th=3):
+    masks, boxes, pred_cls = get_prediction(img_path, threshold)
+    img = cv2.cvtColor(np.array(img_path), cv2.COLOR_BGR2RGB)
+    for i in range(len(masks)):
+        rgb_mask = random_colour_masks(masks[i])
+        img = cv2.addWeighted(img, 1, rgb_mask, 0.5, 0)
+        # Convert to integer if necessary
+        pt1 = (int(boxes[i][0][0]), int(boxes[i][0][1]))
+        # Convert to integer if necessary
+        pt2 = (int(boxes[i][1][0]), int(boxes[i][1][1]))
+        cv2.rectangle(img, pt1, pt2, color=(0, 255, 0), thickness=rect_th)
+        cv2.putText(img, pred_cls[i], pt1, cv2.FONT_HERSHEY_SIMPLEX,
+                    text_size, (0, 255, 0), thickness=text_th)
+    return img
